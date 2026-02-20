6d9b331ad187e6ec46b2c4983fd58e59351693f6
Joao Lucas <joaolucasbernardes@gmail.com>
2026-02-20 15:11:51 -0300

    feat: cria classe principal do spring boot

---
+++ src/main/java/com/joaolucas/analisadorcurriculos/AnalisadorCurriculosApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.analisadorcurriculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisadorCurriculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisadorCurriculosApplication.class, args);
+	}
+
+}
